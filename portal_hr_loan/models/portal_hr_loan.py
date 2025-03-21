81750a05724da0dd87af6616c14d35e8be4f8dd1
seralkhatem3210 <seralkhatem3210@gmail.com>
2025-03-21 04:52:08 +0300

    init zabia

--- portal_hr_loan/models/portal_hr_loan.py
+++ portal_hr_loan/models/portal_hr_loan.py
@@ -4,7 +4,7 @@ class HrContractHistory(models.Model):
     _name = "hr.contract.history"
     _description = "Contract History"
 
-    time_credit = fields.Float(string="Time Credit", default=0.0)  # ✅ Ensure this field exists
+    # time_credit = fields.Float(string="Time Credit", default=0.0)  # ✅ Ensure this field exists
 
 class HrLeaveType(models.Model):
     _inherit = "hr.leave.type"
