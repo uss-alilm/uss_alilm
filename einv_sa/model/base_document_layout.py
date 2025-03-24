9ba92f6ebe37b85464baab77a2b3976311b9fe21
seralkhatem3210 <seralkhatem3210@gmail.com>
2025-03-24 22:53:13 +0300

    update einv_sa

--- einv_sa/model/base_document_layout.py
+++ einv_sa/model/base_document_layout.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, Warning
+from odoo.exceptions import UserError
 
 
 class BaseDocumentLayout(models.TransientModel):
